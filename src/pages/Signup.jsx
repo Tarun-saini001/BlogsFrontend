20f9338a91fc4545d33f1255c995fef2ae88bdc1
Tarun Saini <sainitarun7888@gmail.com>
2026-02-16 00:53:11 +0530

    signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Signup = () => {
+    const API = import.meta.env.VITE_API_URL;
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -12,11 +13,27 @@ const Signup = () => {
             [e.target.name]: e.target.value
         });
     }
-    // const handleRegister = async(formData)={
-    //     if(!formData.email && !formData.password){
-    //         return,
-    //     }
-    // }
+    const handleRegister = async () => {
+        try {
+            const response = await fetch(`${API}/onBoarding/user/register`,{
+                    method:"POST",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify(formData)
+            });
+            const data = await response.json();
+            if(response.ok){
+                alert("User registered successfully!")
+                console.log("data",data);
+            }else{
+                alert(data.message || "Registration failed")
+            }
+        } catch (error) {
+            console.log(error);
+            alert("something went wrong")
+        }
+    };``
     return (
         <div className=' h-screen flex justify-center items-center'>
             <div className='bg-gray-200 hover:w-[42%] hover:h-[62%] hover:shadow-gray-500  w-[40%] h-[60%] text-black  rounded-3xl flex flex-col space-y-5 items-center'>
@@ -25,6 +42,7 @@ const Signup = () => {
                     <p className='bg-white  w-[25%] text-black rounded p-1 text-center'>Name</p>
                     <input
                         type="text"
+                        name="name"
                         placeholder="userName"
                         onChange={handleChange}
                         className='bg-white text-gray-500 w-[60%]   rounded' />
@@ -32,6 +50,7 @@ const Signup = () => {
                 <div className='flex space-x-2   w-[80%] justify-between '>
                     <p className='bg-white  w-[25%] text-black rounded p-1 text-center'>Email</p>
                     <input type="text"
+                        name="email"
                         placeholder='Enter Email'
                         onChange={handleChange}
                         className='bg-white text-gray-400 w-[60%] rounded' />
@@ -39,11 +58,12 @@ const Signup = () => {
                 <div className='flex space-x-  w-[80%] justify-between'>
                     <p className='bg-white w-[25%] text-center text-black rounded p-1'>Password</p>
                     <input type="text"
+                        name="password"
                         placeholder='Enter Password'
                         onChange={handleChange}
                         className='bg-white text-gray-400 w-[60%] rounded' />
                 </div>
-                <button onClick={()=>handleRegister(formData)} className='bg-black text-white mt-[8%] rounded p-1 w-[40%]'>Register</button>
+                <button onClick={() => handleRegister(formData)} className='bg-black text-white mt-[8%] rounded p-1 w-[40%]'>Register</button>
             </div>
         </div>
     )
