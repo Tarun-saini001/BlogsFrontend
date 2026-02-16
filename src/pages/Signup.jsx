7434b5e2c495f850b1369852ccf66fef4579c548
Tarun Saini <sainitarun7888@gmail.com>
2026-02-16 11:55:48 +0530

    login validations

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Signup = () => {
     const API = import.meta.env.VITE_API_URL;
+    const [errors, setErrors] = useState({});
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -15,28 +16,40 @@ const Signup = () => {
     }
     const handleRegister = async () => {
         try {
-            const response = await fetch(`${API}/onBoarding/user/register`,{
-                    method:"POST",
-                    headers:{
-                        "Content-Type":"application/json"
-                    },
-                    body:JSON.stringify(formData)
+            const response = await fetch(`${API}/onBoarding/user/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
             });
             const data = await response.json();
-            if(response.ok){
+            if (response.ok) {
                 alert("User registered successfully!")
-                console.log("data",data);
-            }else{
-                alert(data.message || "Registration failed")
+                console.log("data", data);
+                setErrors({});
+            } else {
+                console.log('data:--- ', data);
+                if (data.errors) {
+                    const formattedErrors = {};
+                    data.errors.forEach(err => {
+                        formattedErrors[err.field] = err.message;
+                    });
+                    setErrors(formattedErrors);
+                    console.log('formattedErrors: ', formattedErrors);
+                } else {
+                    alert(data.message || "Registration failed");
+                }
+
             }
         } catch (error) {
             console.log(error);
             alert("something went wrong")
         }
-    };``
+    };
     return (
         <div className=' h-screen flex justify-center items-center'>
-            <div className='bg-gray-200 hover:w-[42%] hover:h-[62%] hover:shadow-gray-500  w-[40%] h-[60%] text-black  rounded-3xl flex flex-col space-y-5 items-center'>
+            <div className='bg-gray-200  w-[40%] h-auto text-black  rounded-3xl pb-6 flex flex-col space-y-5 items-center'>
                 <p className='text-4xl p-5'>Signup</p>
                 <div className='flex space-x-2   w-[80%] justify-between '>
                     <p className='bg-white  w-[25%] text-black rounded p-1 text-center'>Name</p>
@@ -46,7 +59,14 @@ const Signup = () => {
                         placeholder="userName"
                         onChange={handleChange}
                         className='bg-white text-gray-500 w-[60%]   rounded' />
+
                 </div>
+                {errors.name && (
+                    <span className="text-red-500 text-sm  ml-auto w-[60%]">
+                        {errors.name}
+                    </span>
+                )}
+
                 <div className='flex space-x-2   w-[80%] justify-between '>
                     <p className='bg-white  w-[25%] text-black rounded p-1 text-center'>Email</p>
                     <input type="text"
@@ -55,6 +75,12 @@ const Signup = () => {
                         onChange={handleChange}
                         className='bg-white text-gray-400 w-[60%] rounded' />
                 </div>
+                {errors.email && (
+                    <span className="text-red-500 text-sm  ml-auto w-[60%]">
+                        {errors.email}
+                    </span>
+                )}
+
                 <div className='flex space-x-  w-[80%] justify-between'>
                     <p className='bg-white w-[25%] text-center text-black rounded p-1'>Password</p>
                     <input type="text"
@@ -63,6 +89,12 @@ const Signup = () => {
                         onChange={handleChange}
                         className='bg-white text-gray-400 w-[60%] rounded' />
                 </div>
+                {errors.password && (
+                    <span className="text-red-500 text-sm  ml-auto w-[60%]">
+                        {errors.password}
+                    </span>
+                )}
+
                 <button onClick={() => handleRegister(formData)} className='bg-black text-white mt-[8%] rounded p-1 w-[40%]'>Register</button>
             </div>
         </div>
