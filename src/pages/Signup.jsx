f1d11324e685056269dd7118283a5f56983dfc11
Tarun Saini <sainitarun7888@gmail.com>
2026-02-18 11:15:38 +0530

    verify otp

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,51 +1,42 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
+
 const Signup = () => {
     const navigate = useNavigate();
     const { login } = useContext(AuthContext);
     const API = import.meta.env.VITE_API_URL;
+
     const [errors, setErrors] = useState({});
+    const [showOtpField, setShowOtpField] = useState(false);
+    const [otp, setOtp] = useState("");
+    const [tempEmail, setTempEmail] = useState("");
+
     const [formData, setFormData] = useState({
         name: "",
         email: "",
         password: "",
         confirmPassword: ""
-    })
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-
-        const updatedForm = {
-            ...formData,
-            [name]: value
-        };
-
-        setFormData(updatedForm);
-        setErrors(validate(updatedForm));
-    };
+    });
 
 
     const validate = (data) => {
         const newErrors = {};
 
-        // name validation
         if (!data.name.trim()) {
             newErrors.name = "Name is required";
+        } else if (!/^[A-Za-z\s]+$/.test(data.name.trim())) {
+            newErrors.name = "Name must contain only letters";
         } else if (data.name.trim().length < 3) {
             newErrors.name = "Name must be at least 3 characters";
         }
 
-        // email validation
         if (!data.email.trim()) {
             newErrors.email = "Email is required";
-        } else if (
-            !/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.com$/.test(data.email)
-        ) {
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
             newErrors.email = "Invalid email format";
         }
 
-
-        // password validation
         if (!data.password.trim()) {
             newErrors.password = "Password is required";
         } else if (data.password.length < 6) {
@@ -61,13 +52,140 @@ const Signup = () => {
         return newErrors;
     };
 
-    const handleRegister = async () => {
 
+    const validateOtp = (value) => {
+        const newErrors = {};
+
+        if (!value.trim()) {
+            newErrors.otp = "OTP is required";
+        } else if (!/^\d{4}$/.test(value)) {
+            newErrors.otp = "OTP must be 4 digits";
+        }
+
+        return newErrors;
+    };
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        const updatedForm = {
+            ...formData,
+            [name]: value
+        };
+
+        setFormData(updatedForm);
+        setErrors(validate(updatedForm));
+    };
+
+
+    const handleRegister = async () => {
         const validationErrors = validate(formData);
         if (Object.keys(validationErrors).length > 0) {
             setErrors(validationErrors);
             return;
         }
+
+        try {
+            const response = await fetch(`${API}/onBoarding/user/signup`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    name: formData.name,
+                    email: formData.email,
+                    password: formData.password
+                }),
+                credentials: "include"
+            });
+
+            const data = await response.json();
+            console.log("signup response:", data);
+
+            if (response.ok && data.statusCode === 200) {
+                setTempEmail(formData.email);
+                setShowOtpField(true);
+                console.log("showOtpField",showOtpField);
+            } else if (data.status === "validation" && data.message?.includes("already verified")) {
+                alert("This email is already registered ");
+            } else {
+                alert(data.message || "Registration failed");
+            }
+        } catch (error) {
+            console.error("Signup error:", error);
+            alert("Something went wrong. Please try again later.");
+        }
+    };
+
+
+
+    const handleOtpChange = (e) => {
+        const value = e.target.value;
+        setOtp(value);
+
+        const otpErrors = validateOtp(value);
+
+        setErrors((prev) => ({
+            ...prev,
+            ...otpErrors
+        }));
+    };
+
+
+
+    const handleVerifyOtp = async () => {
+        const otpErrors = validateOtp(otp);
+
+        if (Object.keys(otpErrors).length > 0) {
+            setErrors((prev) => ({
+                ...prev,
+                ...otpErrors
+            }));
+            return;
+        }
+
+        try {
+            console.log('tempEmail: ', tempEmail);
+            console.log('otp: ', otp);
+            console.log('formData.name: ', formData.name);
+            const response = await fetch(`${API}/onBoarding/user/verifyOtp`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: tempEmail,
+                    otp: Number(otp),
+                    otpType: 1,
+                    name: formData.name,
+                    password: formData.password,
+                }),
+                credentials: "include"
+            });
+
+            const data = await response.json();
+            console.log('data: ', data);
+
+            if (response.ok) {
+                if (data?.data?.token) {
+                    login(data.data.token);
+                    navigate("/");
+                }
+            } else {
+                setErrors((prev) => ({
+                    ...prev,
+                    otp: data.message || "Invalid OTP"
+                }));
+            }
+
+        } catch (error) {
+            console.log(error);
+            alert("Something went wrong");
+        }
+    };
+
+
+
+    const handleResendOtp = async () => {
         try {
             const response = await fetch(`${API}/onBoarding/user/register`, {
                 method: "POST",
@@ -77,156 +195,149 @@ const Signup = () => {
                 body: JSON.stringify({
                     name: formData.name,
                     email: formData.email,
-                    password: formData.password,
+                    password: formData.password
                 }),
-                credentials: 'include'
+                credentials: "include"
             });
+
             const data = await response.json();
+
             if (response.ok) {
-                if (response.ok) {
-
-                    console.log("data", data);
-
-                    if (data?.data?.token) {
-                        login(data.data.token);
-                        alert("Login successful!");
-                        navigate("/");
-                    } else {
-                        alert(data.message || "Operation completed");
-                    }
-                }
-
+                alert("OTP resent successfully!");
             } else {
-                console.log('data:--- ', data);
-                if (data.errors) {
-                    const formattedErrors = {}
-                    data.errors.forEach(err => {
-                        formattedErrors[err.field] = err.message;
-                    });
-                    setErrors(formattedErrors);
-                    console.log('formattedErrors: ', formattedErrors);
-                } else {
-                    alert(data.message || "Registration failed");
-                }
-
+                alert(data.message || "Failed to resend OTP");
             }
         } catch (error) {
-            console.log(error);
-            alert("something went wrong")
+            alert("Something went wrong");
         }
     };
+
+
+
     return (
-        <div className='min-h-screen flex justify-center items-center px-4'>
-            <div className='w-full max-w-md text-black shadow-2xl rounded-3xl py-8 flex flex-col items-center gap-5'>
+        <div className="min-h-screen flex justify-center items-center px-4">
+            <div className="w-full max-w-md text-black shadow-2xl rounded-3xl py-8 flex flex-col items-center gap-5">
 
+                <p className="text-4xl">Register</p>
 
-                <p className='text-4xl '>Register</p>
+                {/* name */}
                 <div className="w-[80%] flex flex-col space-y-1">
-                    <label className="text-sm font-medium text-left">Name</label>
+                    <label>Name</label>
                     <input
                         type="text"
                         name="name"
-                        placeholder='Enter Name'
+                        placeholder="Enter name"
+
                         value={formData.name}
                         onChange={handleChange}
-                        className={`bg-white px-3 py-2 border rounded focus:outline-none focus:ring-2 ${errors.name
-                            ? "border-red-500 focus:ring-red-200"
-                            : "border-gray-300 focus:ring-blue-500"
-                            }`}
+                        disabled={showOtpField}
+                        className="border px-3 py-2 rounded"
                     />
-                    {errors.name && (
-                        <span className="text-red-500 text-xs">{errors.name}</span>
-                    )}
+                    {errors.name && <span className="text-red-500 text-xs">{errors.name}</span>}
                 </div>
 
-
+                {/* email */}
                 <div className="w-[80%] flex flex-col space-y-1">
-                    <label className="text-sm font-medium text-left">Email</label>
+                    <label>Email</label>
                     <input
                         type="email"
                         name="email"
-                        placeholder='Enter Email'
+                        placeholder="Enter email"
                         value={formData.email}
                         onChange={handleChange}
-                        className={`bg-white px-3 py-2 border rounded focus:outline-none focus:ring-2 ${errors.email
-                            ? "border-red-500 focus:ring-red-200"
-                            : "border-gray-300 focus:ring-blue-500"
-                            }`}
+                        disabled={showOtpField}
+                        className="border px-3 py-2 rounded"
                     />
-                    {errors.email && (
-                        <span className="text-red-500 text-xs">{errors.email}</span>
-                    )}
+                    {errors.email && <span className="text-red-500 text-xs">{errors.email}</span>}
                 </div>
 
-
+                {/* password */}
                 <div className="w-[80%] flex flex-col space-y-1">
-                    <label className="text-sm font-medium text-left">Password</label>
+                    <label>Password</label>
                     <input
                         type="password"
                         name="password"
-                        placeholder='Enter Password'
+                        placeholder="Enter password"
+
                         value={formData.password}
                         onChange={handleChange}
-                        className={`bg-white px-3 py-2 border rounded focus:outline-none focus:ring-2 ${errors.password
-                            ? "border-red-500 focus:ring-red-200"
-                            : "border-gray-300 focus:ring-blue-500"
-                            }`}
+                        disabled={showOtpField}
+                        className="border px-3 py-2 rounded"
                     />
-                    {errors.password && (
-                        <span className="text-red-500 text-xs">{errors.password}</span>
-                    )}
+                    {errors.password && <span className="text-red-500 text-xs">{errors.password}</span>}
                 </div>
 
+                {/* confirm password */}
                 <div className="w-[80%] flex flex-col space-y-1">
-                    <label className="text-sm font-medium text-left">
-                        Confirm Password
-                    </label>
+                    <label>Confirm Password</label>
                     <input
                         type="password"
                         name="confirmPassword"
-                        placeholder="Confirm Password"
+                        placeholder="Confirm password"
+
                         value={formData.confirmPassword}
                         onChange={handleChange}
-                        className={`bg-white px-3 py-2 border rounded focus:outline-none focus:ring-2 ${errors.confirmPassword
-                            ? "border-red-500 focus:ring-red-200"
-                            : "border-gray-300 focus:ring-blue-500"
-                            }`}
+                        disabled={showOtpField}
+                        className="border px-3 py-2 rounded"
                     />
                     {errors.confirmPassword && (
-                        <span className="text-red-500 text-xs">
-                            {errors.confirmPassword}
-                        </span>
+                        <span className="text-red-500 text-xs">{errors.confirmPassword}</span>
                     )}
                 </div>
 
 
-                <button
-                    disabled={Object.keys(errors).length > 0}
-                    onClick={handleRegister}
-                    className={`mt-[8%] rounded p-1 w-[40%] ${Object.keys(errors).length > 0
-                            ? "bg-gray-400 cursor-not-allowed"
-                            : "bg-black text-white cursor-pointer"
-                        }`}
-                >
-                    Register
-                </button>
-
-
-                <div className="w-[80%] text-center  text-sm">
-                    <span className="text-gray-600 ">
-                        Already have an account?{" "}
-                    </span>
-                    <Link
-                        to="/login"
-                        className="text-blue-600 cursor-pointer  font-medium"
+                {!showOtpField && (
+                    <button
+                        onClick={handleRegister}
+                        className="bg-black text-white px-4 py-2 rounded"
                     >
+                        Register
+                    </button>
+                )}
+
+
+                {showOtpField && (
+                    <div className="w-[80%] flex flex-col space-y-2">
+                        <label>Enter OTP</label>
+                        <input
+                            type="text"
+                            maxLength={4}
+                            value={otp}
+                            onChange={handleOtpChange}
+                            className="border px-3 py-2 rounded"
+                        />
+                        {errors.otp && (
+                            <span className="text-red-500 text-xs">{errors.otp}</span>
+                        )}
+
+                        <div className="flex justify-between">
+                            <button
+                                onClick={handleVerifyOtp}
+                                className="bg-black text-white px-3 py-1 rounded"
+                            >
+                                Verify OTP
+                            </button>
+
+                            <button
+                                onClick={handleResendOtp}
+                                className="text-blue-600 text-sm"
+                            >
+                                Resend OTP
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+                <div className="text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-600">
                         Login
                     </Link>
                 </div>
 
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Signup
+export default Signup;
